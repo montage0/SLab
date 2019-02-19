670013cd30adb22588f2cce03ec035b15bfe8127
Glitch (massive-hearing) <none>
2019-02-19 00:25:54 +0000

    🏌📷 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello, WebVR! • A-Frame</title>
+    <meta name="description" content="Hello, WebVR! • A-Frame">
+    <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
+  </head>
+  <body>
+    <a-scene background="color: #f5d4d7">
+      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#dc0032" shadow></a-box>
+      <a-box position="-1 0.25 -3" rotation="0 15 0" color="#dc0032" shadow></a-box>
+      <a-sphere position="0 1.25 -5" radius="1.25" color="#ffd700" shadow></a-sphere>
+      <a-sphere position="-2 2.55 -4" radius="0.40" color="#fab6c4" shadow></a-sphere>
+      <a-cylinder position="1 1 -3" radius="0.10" height="4.5" color="#c1b561" shadow></a-cylinder>
+      <a-plane position="0 0.8 -4" rotation="-90 0 0" width="5" height="4" color="#e2b7a6" shadow></a-plane>
+    </a-scene>
+  </body>
+</html>
